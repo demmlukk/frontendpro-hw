8272ea5fb8a45bae90e6bc0cba1eb8ee5fa05147
demmluk <demmluk@gmail.com>
2024-01-17 00:11:34 +0100

    hw-2 done

---
+++ hw-2.js
@@ -0,0 +1,14 @@
+let number1 = +prompt('Enter the first number');
+console.log(number1);
+
+let number2 = +prompt('Enter the second number');
+console.log(number2);
+
+let number3 = +prompt('Enter the third number');
+console.log(number3);
+
+let result = (number1 + number2 + number3) / 3;
+alert("Arithmetic mean of the entered numbers is " + result);
+console.log(result);
+
+
