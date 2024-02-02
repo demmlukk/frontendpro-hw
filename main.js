3d29a857cc45cd7be871a8610eae3b4f9dfd6e2d
demmluk <demmluk@gmail.com>
2024-02-02 18:32:32 +0100

    hw-9 corrected

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+function removeElement (arr, el) {
+    let index = arr.indexOf(el);
+    arr.splice(index, 1);
+}
+
 let array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
 array.splice(4, 1);
 
 console.log(array);
