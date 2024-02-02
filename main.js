9dbc4d44e4eca2fc30060c3da9f550d5b9dd6a28
demmluk <demmluk@gmail.com>
2024-02-02 20:26:10 +0100

    hw-12 done

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 function powerRecursive(num, degree) {
-    // Базовий випадок: якщо ступінь дорівнює 0, повертаємо 1
     if (degree === 0) {
         return 1;
     }
-    // Рекурсивний випадок: зводимо число в попередній ступінь та множимо на саме число
     else {
         return num * powerRecursive(num, degree - 1);
     }
 }
 
-// Приклад використання
-let result = powerRecursive(2, 3);  // Зводимо 2 до ступеня 3
+let result = powerRecursive(5, 3);
 console.log(result);
