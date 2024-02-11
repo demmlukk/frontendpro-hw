2c2b9ade18f20b462b90ce30a1df7765099e2eee
demmlukk <demmluk@gmail.com>
2024-02-11 17:48:50 +0100

    hw-15 done

---
+++ main.js
@@ -0,0 +1,90 @@
+// 1.
+function calculateAverageOfNumbers(array) {
+    const numElements = array.filter (element => typeof element === 'number' && !isNaN(element));
+    if (numElements.length === 0) {
+        return null;
+    }
+    const average = numElements.reduce((sum, element) => sum + element, 0) / numElements.length;
+    return average;
+}
+
+
+
+// 2.
+ function doMath (x, znak, y) {
+    x = +x;
+    y = +y;
+    if (isNaN(x) || isNaN(y)) {
+        return "Будь ласка, введіть коректні числа."
+    }
+
+    switch (znak) {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            if (y === 0) {
+                return "Ділення на нуль неможливе.";
+            }
+            return x / y;
+        case '%':
+            return x % y;
+        case '^':
+            return Math.pow(x, y);
+        default:
+            return "Невірний знак операції. Введіть один із: +, -, *, /, %, ^";
+
+    }
+ }
+
+
+
+
+ // 3.
+function fillTwoDimensionalArray() {
+    const rows = +(prompt("Enter the number of rows:"));
+    const columns = +(prompt("Enter the number of columns:"));
+
+    if (isNaN(rows) || isNaN(columns) || rows <= 0 || columns <= 0) {
+        return "Please enter valid values for the array dimensions.";
+    }
+
+    const twoDimensionalArray = [];
+
+    for (let i = 0; i < rows; i++) {
+        const row = [];
+        for (let j = 0; j < columns; j++) {
+            const value = prompt(`Enter the value for element [${i}][${j}]:`);
+            row.push(value);
+        }
+        twoDimensionalArray.push(row);
+    }
+
+    return twoDimensionalArray;
+}
+
+const result = fillTwoDimensionalArray();
+console.log(result);
+
+
+
+
+//4.
+function removeCharacters(inputString, charactersToRemove) {
+    const charsSet = new Set(charactersToRemove);
+
+    const resultString = inputString.split('').filter(char => !charsSet.has(char)).join('');
+
+    return resultString;
+}
+
+const userInputString = prompt("Enter a string:");
+const charactersToRemoveInput = prompt("Enter characters to remove (comma-separated):");
+
+const charactersToRemove = charactersToRemoveInput.split(',');
+
+const result = removeCharacters(userInputString, charactersToRemove);
+console.log(result);
