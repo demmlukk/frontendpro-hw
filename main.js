dd73f5eac79e7d7095ead9249913234c7b0ea686
demmlukk <demmluk@gmail.com>
2024-02-24 23:38:39 +0200

    hw-17 done

---
+++ main.js
@@ -0,0 +1,16 @@
+//1
+let input1 = prompt("Введіть перший рядок:");
+let input2 = prompt("Введіть другий рядок:");
+let input3 = prompt("Введіть третій рядок:");
+
+let result = input2 + " " + input3 + " " + input1;
+console.log(result);
+
+
+//2
+let numberInput = prompt("Введіть п'ятизначне число:");
+let number = +(numberInput);
+
+let digits = number.toString().split("").join(" ");
+
+console.log(digits);
